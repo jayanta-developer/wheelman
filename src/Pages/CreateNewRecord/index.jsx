edbeee1884d19be96976888648ef1adc794b2e90
jayanta dey <116143724+jayanta-musquare@users.noreply.github.com>
2023-11-24 18:48:16 +0530

    fix popup text

--- src/Pages/CreateNewRecord/index.jsx
+++ src/Pages/CreateNewRecord/index.jsx
@@ -329,7 +329,7 @@ export default function CreateNewRecord() {
 
             <div className="row">
               <div className="col-md-5 col-lg-4">
-                <p>
+                <p className="notecard-type">
                   Select Notecard Type: Maintenance, Modification, Restoration,
                   or Other
                 </p>
